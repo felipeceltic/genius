e42780ea01241482dcfd65b495ea87e164ccc9b5
Luiz Felipe da Silva Araujo <felipeceltic@gmail.com>
2023-07-07 15:06:12 -0300

    :tada: Laravel Jetstream Bootstrap

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Projeto Laravel</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1>Exemplo de Uso do Bootstrap</h1>
+        <button class="btn btn-primary">Botão</button>
+    </div>
+    
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
